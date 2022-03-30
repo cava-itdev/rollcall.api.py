2764d6c918e848247b7f3f849fb9000ee7b0e592
cava <cava.itdev@gmail.com>
2022-03-30 16:38:12 +0200

    Add member class

--- rollcall/helpers.py
+++ rollcall/helpers.py
@@ -1,14 +1,22 @@
 from rollcall import app, members
+from rollcall.member import Member
 from os import path, getcwd, makedirs
 from glob import glob
 from csv import DictReader
-
+import datetime as dt
 
 def setupDirs():
     dataDir = path.join(getcwd(), 'data')
     app.config['DATA'] = dataDir
     makedirs(dataDir, exist_ok=True)
     makedirs(path.join(dataDir, 'faces'), exist_ok=True)
+
+
+def createRegister():
+    today = dt.datetime.today()
+    register = path.join(app.config['DATA'], f'{today.year}-{today.month:02}-{today.day:02}.txt') 
+    with open(register, 'w') as f:
+        pass
 
 
 def getMembers():
@@ -20,13 +28,12 @@ def getMembers():
                 memberId = row['Mem No'].replace('\t','')
                 memberId = f'{int(memberId):06d}'
                 if not memberId in members.keys():
-                    members[memberId] = { 
-                        'id': memberId,
-                        'altid': row['SA Identity No'].replace('\t',''),
-                        'name': row['Preferred Name'].replace('\t',''),
-                        'surname': row['Surname'].replace('\t',''),
-                        'language': row['Language'].replace('\t','')[0:1],
-                        'email': row['E-mail address'].replace('\t',''),
-                        'phone': row['Mobile No'].replace('\t','')
-                        }
-
+                    member = Member()
+                    member.id = memberId
+                    member.altId = row['SA Identity No'].replace('\t','')
+                    member.name = row['Preferred Name'].replace('\t',''),
+                    member.surname = row['Surname'].replace('\t',''),
+                    member.language = row['Language'].replace('\t','')[0:1],
+                    member.email = row['E-mail address'].replace('\t',''),
+                    member.phone = row['Mobile No'].replace('\t','')
+                    members[memberId] = member
---
+++ rollcall/member.py
@@ -0,0 +1,11 @@
+class Member:
+    id: str
+    altId: str
+    name: str
+    surname: str
+    language: str
+    email: str
+    phone: str
+
+    def __init__(self):
+        self.data = []
